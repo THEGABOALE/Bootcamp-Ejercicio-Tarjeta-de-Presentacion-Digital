cd75a741a355997fcdf3b622b1ac43e65c353763
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-12 23:10:56 -0600

    borde creado, probando aún

---
+++ scripts/animacion.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const card = document.getElementById("card");
+  
+    if (!card) return;
+  
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("card-wrapper");
+  
+    const border = document.createElement("div");
+    border.classList.add("card-border");
+  
+    // Insertar el wrapper en el DOM
+    card.parentNode.insertBefore(wrapper, card);
+    wrapper.appendChild(card);
+    wrapper.appendChild(border);
+  });
+  
